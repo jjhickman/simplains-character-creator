adb047258acb8b90a1d53ecb1e1fb19a31df9e87
Joshua Hickman <me@jjhickman.com>
2025-03-28 13:57:07 -0400

    Starting...

---
+++ server/character-creator/src/main/java/com/simplains/character_creator/CharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.simplains.character_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterCreatorApplication.class, args);
+	}
+
+}
---
+++ server/character-creator/src/test/java/com/simplains/character_creator/CharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplains.character_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
